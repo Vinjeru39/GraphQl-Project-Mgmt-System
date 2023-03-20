4cf75d967cd62c0b7993b7636442779dc3ff3381
Vinjeru Msowoya <vmsowoya@must.ac.mw>
2023-03-21 01:58:45 +0200

    Completed Project before deploying modifications

---
+++ client/src/mutations/clientMutations.js
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+const ADD_CLIENT = gql`
+  mutation addClient($name: String!, $email: String!, $phone: String!) {
+    addClient(name: $name, email: $email, phone: $phone) {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+//The first one specifies that is an id of type if and the second one calls it and passes tha id in
+const DELETE_CLIENT = gql`
+  mutation deleteClient($id: ID!) {
+    deleteClient(id: $id) {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+export { ADD_CLIENT, DELETE_CLIENT };
---
+++ client/src/mutations/projectMutations.js
@@ -0,0 +1,67 @@
+import { gql } from "@apollo/client";
+
+//ProjectStatus! is the name of the enum type
+const ADD_PROJECT = gql`
+  mutation addProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+const DELETE_PROJECT = gql`
+  mutation deleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      id
+    }
+  }
+`;
+
+//ProjectStatusUpdate is the name of the enum set in the backend
+const UPDATE_PROJECT = gql`
+  mutation updateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { ADD_PROJECT, DELETE_PROJECT, UPDATE_PROJECT };
