a1e78caec49b6bb07d4c6b3d72a9335c6ceb2a6a
Vinjeru Msowoya <vmsowoya@must.ac.mw>
2023-03-21 02:32:53 +0200

    To deploy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
@@ -18,8 +19,27 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: (process.env.NODE_ENV = "development"),
+    graphiql: true, // Initially was -> (process.env.NODE_ENV = "development"),
+    //But with the above it only serves the static folders in development mode
   })
 );
+
+// Serve frontend -> Added for serving
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get(
+    "*",
+    (
+      req,
+      res //* for any route aprt from our api routes up there ofcourse
+    ) =>
+      res.sendFile(
+        path.resolve(__dirname, "../", "client", "build", "index.html")
+      )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, console.log(`Server running on port ${port}`));
